453a02b85a5b8c7fe160a31de99599d8e8ba1bba
peng <1084936206@qq.com>
2021-02-23 17:43:42 +0800

    修改1

--- src/main/java/com/atguigu/jianfa/bixiejianfa.java
+++ src/main/java/com/atguigu/jianfa/bixiejianfa.java
@@ -5,6 +5,9 @@ public class bixiejianfa {
         System.out.println("辟邪剑法好!!");
         System.out.println("辟邪剑法好!!");
         System.out.println("辟邪剑法好!!");
-        System.out.println("辟邪剑法好!!");
+        System.out.println("六脉神剑棒!!");
+        System.out.println("六脉神剑棒!!");
+        System.out.println("六脉神剑棒!!");
+        System.out.println("六脉神剑棒!!");
     }
 }
